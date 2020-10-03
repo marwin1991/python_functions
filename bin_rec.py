1d2fc5162681320abb83a659e21dd04a465f8adf
marwin1991 <peter.zmilczak@gmail.com>
2020-10-03 14:05:52 +0200

    recurrence with return

--- bin_rec.py
+++ bin_rec.py
@@ -1,5 +1,3 @@
-
-
 def dec_to_bin(number):
     print("Aktualna liczba " + str(number))
     if number > 1:
@@ -7,4 +5,16 @@ def dec_to_bin(number):
     print("BINARNIE: " + str(number%2))
 
 
-dec_to_bin(79)
+def dec_to_bin_with_return(number):
+    child_reposne = ""
+    if number > 1:
+        child_reposne = dec_to_bin_with_return(number//2)
+    return child_reposne + str(number%2)
+
+print(dec_to_bin(79))
+
+
+### Piotr --> rtoiP
+def reverse_string(to_revers):
+    """TODO"""
+    pass
