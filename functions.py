e1647fab9b0a4d624808a9d4b1e86bbccb4f2aa0
marwin1991 <peter.zmilczak@gmail.com>
2020-10-03 13:43:16 +0200

    Added simple keywords functions

---
+++ functions.py
@@ -0,0 +1,45 @@
+import os
+
+def get_init_state():
+    return {
+        "mouse": 20,
+        "keyboard": 15,
+        "laptop": 3,
+        "gpu": 10
+    }
+
+def clear_consol():
+    os.system("cls || clear")
+
+def print_state(state, end="===============", header="Current state: \n===============", should_clear_consol=False):
+    if should_clear_consol:
+        clear_consol()
+    
+    print(header)
+    for key, value in state.items():
+        print(f"{key}: {value}")
+    print(end)
+
+
+def add_new_items_to_state(state, items_to_add):
+    print("Dodawanie itemów do stanu: " + str(items_to_add))
+    if items_to_add[0] in state:
+        state[items_to_add[0]] += 1
+    else:
+         state[items_to_add[0]] = 1
+    
+    if len(items_to_add) > 1:
+        items_to_add.remove(items_to_add[0])
+        add_new_items_to_state(state, items_to_add)
+    
+
+
+def main():
+    current_state = get_init_state()
+    print_state(current_state)
+    print_state(current_state, should_clear_consol=True)
+    add_new_items_to_state(current_state, ["gpu", "cpu", "camera", "gpu"])
+    print_state(current_state)
+
+if __name__ == "__main__":
+    main()
