d8fadc7725c903ec6ab4ae6c476d5ac7395f510f
marwin1991 <peter.zmilczak@gmail.com>
2020-10-03 13:53:05 +0200

    reccursion

--- bin_rec.py
+++ bin_rec.py
@@ -1,3 +1,10 @@
 
 
-def 
+def dec_to_bin(number):
+    print("Aktualna liczba " + str(number))
+    if number > 1:
+        dec_to_bin(number//2)
+    print("BINARNIE: " + str(number%2))
+
+
+dec_to_bin(79)
